36768168255d3966bec736287d7a6838eb77d7ee
Mahdi Erfanian <mahdi.erfanian@divar.ir>
2022-02-25 16:49:18 +0330

    Modify app.js

--- src/App.js
+++ src/App.js
@@ -6,16 +6,38 @@ import ControlledAnimation from "./ControlledAnimation";
 class App extends Component {
   state = {
     position: 0,
-    ornamentPosition:1000
+    ornamentPosition: 1000,
+    loading: true,
+    data: "",
   };
   componentDidMount() {
+    const id = window.location.pathname;
+    const url = "http://127.0.0.1:3000" + id;
+    console.log(url);
+
+    const that = this;
+    fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (jsonData) {
+        return JSON.stringify(jsonData);
+      })
+      .then(function (jsonStr) {
+        that.setState({ data: JSON.parse(jsonStr), loading: false });
+        console.log(JSON.parse(jsonStr));
+      });
+
     window.addEventListener("scroll", this.listenToScroll);
 
-    setInterval(()=> {
-      if(this.state.position > this.state.ornamentPosition - 1 && this.state.position < 77){
-        window.scrollBy(0,2)
+    setInterval(() => {
+      if (
+        this.state.position > this.state.ornamentPosition - 1 &&
+        this.state.position < 77
+      ) {
+        window.scrollBy(0, 2);
       }
-    }, 1)
+    }, 1);
   }
 
   componentWillUnmount() {
@@ -30,26 +52,38 @@ class App extends Component {
       document.documentElement.scrollHeight -
       document.documentElement.clientHeight;
 
-      const ornamentHeight = document.getElementById('ornament_id').clientHeight;
+    const ornamentHeight = document.getElementById("ornament_id").clientHeight;
     const scrolled = (winScroll / height) * 100;
 
-    console.log(scrolled);
     this.setState({
       position: scrolled,
-      ornamentPosition: ornamentHeight/height*100
+      ornamentPosition: (ornamentHeight / height) * 100,
     });
   };
 
   render() {
+    if (this.state.loading) {
+      return (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      );
+    }
+    let title;
+    if (this.state.data["sex"] == "f") {
+      title = <h1>سرکار خانم {this.state.data["name"]}</h1>;
+    } else {
+      title = <h1>جناب آقای {this.state.data["name"]}</h1>;
+    }
     return (
       <div className="App">
-        <div id='ornament_id' className="ornament">
+        <div id="ornament_id" className="ornament">
           <ControlledAnimation
             name="ornament"
             progress={this.state.position}
             limits={{ start: -1, stop: 6 }}
           />
-        <h1>Dear Mr. Folani</h1>
+          <h1>{title}</h1>
         </div>
         <div
           className="start"
